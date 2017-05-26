714adb479e37d9cdeb1c7295caf54ee4ad38da6d
Lucas Nascimento <lucaspnascimento@hotmail.com>
2017-05-25 21:45:41 -0300

    comit inicial

---
+++ pessoa.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 25/05/2017
+ * Time: 21:09
+ */
+class Pessoa {
+
+    public $nome ;
+    public $idade;
+}
+
+$pessoa1 -> new Pessoa();
+
+$pessoa1->nome ="lucas";
+$pessoa1->idade = "18";
