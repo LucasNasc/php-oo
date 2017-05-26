4b18557bfa7cd9ec802dcc9ab4ad3e439b2e3e46
Lucas Nascimento <lucaspnascimento@hotmail.com>
2017-05-25 21:53:37 -0300

    https://github.com/LucasNasc/php-oo.git Removendo Erros e adicionando nova instancia de pessoa

--- pessoa.php
+++ pessoa.php
@@ -11,7 +11,12 @@ class Pessoa {
     public $idade;
 }
 
-$pessoa1 -> new Pessoa();
+$pessoa1 = new Pessoa();
 
 $pessoa1->nome ="lucas";
 $pessoa1->idade = "18";
+
+$pessoa2 = new Pessoa();
+
+$pessoa2->nome = "Nascimento";
+$pessoa2->idade = "29";
